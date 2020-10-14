1761212ab7e9477a7a95eb0d008bf099034e1035
DanielBoeckelen <72459726+DaniLuk94@users.noreply.github.com>
2020-10-14 17:50:09 +0200

    Update fir_package.vhd

--- fir_package.vhd
+++ fir_package.vhd
@@ -5,59 +5,74 @@ use STD.textio.all;
 use ieee.std_logic_textio.all;
 
 PACKAGE fir_package is
-
-process
+  
   constant nb : integer := 8;
   constant N : integer := 10;
   constant samples_num : integer := 200
     
-  variable row : integer :=0; --useful to take track of the current file row
+PACKAGE BODY fir_package is
 
-  variable v_line : line; --file object
-  variable val : signed(nb-1 downto 0); --temporary variable useful to store x or b_coeff values while building data matrixes
-    
-  variable b_coeff : signed(nb-1 downto 0); --b coefficients
-    
-  type X_array is array (samples_num-1 downto 0) of signed(nb-1 downto 0);
-  type b_coeff is array (N-1 donwto 0) of std_logic_vector(nb-1 downto 0);
+  PROCEDURE LUT_to_samples is
 
-begin
+    variable row : integer :=0; --useful to take track of the current file row
+
+    variable v_line : line; --file object
+    variable val : signed(nb-1 downto 0); --temporary variable useful to store x values while building data matrixes
+
+    type X_array is array (samples_num-1 downto 0) of signed(nb-1 downto 0);
+
+  begin
+
+   --read all lines in the file and write the values in X_array
+    file_open(file_samples, "samples.txt", read_mode); 
+
+    while not endfile(file_samples) loop
+
+      readline(file_samples, v_line);
+      read(v_line, val);
+
+      X_array(row) := val;
+
+      row := row + 1; --next file row
+
+    end loop;
+
+    row := 0; --reset file row
+
+    file_close(file_samples);
+
+    return X_array;
+
+  end LUT_to_samples;
+
+
+  PROCEDURE LUT_to_bcoeff is   
   
- --read all lines in the file and write the values in X_array
-  file_open(file_samples, "samples.txt", read_mode); 
+    variable row : integer :=0; --useful to take track of the current file row
 
-  while not endfile(file_samples) loop
-    
-    readline(file_samples, v_line);
-    read(v_line, val);
+    variable v_line : line; --file object
+    variable val : signed(nb-1 downto 0); --temporary variable useful to store b_coeff values while building data matrixes
 
-    X_array(row) := val;
+    type b_coeff is array (N-1 donwto 0) of std_logic_vector(nb-1 downto 0);
 
-    row := row + 1; --next file row
-    
-  end loop;
+  begin
+   --read all lines in the file and write the values in bcoeff array
+    file_open(file_bcoeff, "bcoeff.txt", read_mode);
 
-  row := 0; --reset file row
+    while not endfile(file_bcoeff) loop
 
-  file_close(file_samples);
+      readline(file_bcoeff, v_line);
+      read(v_line, val);
 
+      b_coeff(row) := val;
 
- --read all lines in the file and write the values in bcoeff array
-  file_open(file_bcoeff, "bcoeff.txt", read_mode);
+      row := row + 1; --next file row
 
-  while not endfile(file_bcoeff) loop
-    
-    readline(file_bcoeff, v_line);
-    read(v_line, val);
+    end loop;
 
-    b_coeff(row) := val;
+    file_close(file_bcoeff);
 
-    row := row + 1; --next file row
-    
-  end loop;
+    return b_coeff;
 
-  file_close(file_bcoeff);
-
-  wait; --to avoid infinite computation in 0 time (allowed in PACKAGE)?
- end process;
+   end LUT_to_bcoeff;
 END PACKAGE;
