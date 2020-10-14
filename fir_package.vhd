984a437fe261bb04f770d51df5690132f169c2b8
emanuelesemino <72786257+emanuelesemino@users.noreply.github.com>
2020-10-14 10:25:12 +0200

    Update fir_package.vhd

--- fir_package.vhd
+++ fir_package.vhd
@@ -6,6 +6,7 @@ PACKAGE fir_package is
   
   constant nb : integer := 8;
   constant N : integer := 10;
-  type b_coefficients is array (9 downto 0) of signed (7 downto 0); --we need to initialize this
+  type b_coefficients is array (9 downto 0) of signed (7 downto 0); 
+  signal b_coeff : b_coefficients := ...; --we need to initialize this
 
 END PACKAGE;
